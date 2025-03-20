b7c1f6227a87d08aed6bd32dae004be2d95ed51d
eerrly <chenerlishuaige@163.com>
2025-03-20 15:23:46 +0800

    提交

---
+++ Assets/Scripts/EEvent.cs
@@ -0,0 +1,7 @@
+public enum EEvent
+{
+    None = 0,
+    AnimStart = 1,
+    Fire = 2,
+    AnimEnd = 3,
+}
---
+++ Assets/Scripts/Editor/AnimationFBXFileHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class AnimationFBXFileHandler
+{
+    [UnityEditor.MenuItem("Tools/处理选中的FBX")]
+    public static void HandleSelected()
+    {
+        var guids = Selection.assetGUIDs;
+        foreach (string guid in guids)
+        {
+            string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+            Debug.Log("通过GUID获取的路径: " + assetPath);
+            if (assetPath.EndsWith(".FBX") || assetPath.EndsWith(".fbx"))
+            {
+                var destDir = Path.GetDirectoryName(assetPath);
+                AnimationUtil.ImportAnimationDataAndClip(assetPath, destDir);
+            }
+        }
+    }
+}
